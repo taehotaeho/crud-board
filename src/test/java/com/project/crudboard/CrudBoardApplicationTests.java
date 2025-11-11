b610d66a80c19e307ef9fd6bc831988276cfcb7d
adina <jackt0506@naver.com>
2025-11-11 23:27:16 +0900

    setting

---
+++ src/test/java/com/project/crudboard/CrudBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.crudboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
